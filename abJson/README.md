3c59fc2b2ed946239506213b7bbfab5483bb285d
AngryBaer <angrybaer@users.noreply.github.com>
2020-07-14 22:06:36 -0700

    adding json wrapper

---
+++ abJson/README.md
@@ -0,0 +1,8 @@
+# abJson
+Simplified version of the json context manager allowing single line read/write operations.
+
+## read
+Return a dictionary object from the given .json file path.
+
+## write
+Write or overwrite a .json file path with the contents of the given dictionary.
---
+++ abJson/api.py
@@ -0,0 +1,49 @@
+"""
+    Wrapper for the json library
+"""
+
+
+# ----------------------------------------------------------------------------------------------- #
+# IMPORTS
+import os
+from json import load, dump
+# ----------------------------------------------------------------------------------------------- #
+
+
+# ----------------------------------------------------------------------------------------------- #
+def read(file_path, mode='r'):
+    """
+    Returns the contents of a .json configuration file.
+
+    :param   file_path:  (str) path to the file
+    :return  json_data: (dict) data contained in the .json file
+    """
+    assert os.path.exists(file_path), 'target path does not exist: \n'  \
+                                      '{}'.format(file_path)
+
+    with open(file_path, mode) as json_file:
+        json_data = load(json_file)
+
+    return json_data
+
+
+def write(file_path, json_data, mode='w+'):
+    """
+    Writes a .json file containing the given data.
+
+    :param  file_path:  (str) absolute path to file
+    :param  json_data: (dict) data contained in the .json file
+    :return          : (bool) confirmation of transaction
+    """
+    target_folder = os.path.dirname(file_path)
+
+    assert os.path.exists(target_folder), 'target folder does not exist: \n'  \
+                                          '{}'.format(target_folder)
+    assert isinstance(json_data, dict),   'requires dict data type, given '  \
+                                          '{}'.format(type(json_data))
+
+    with open(file_path, mode) as json_file:
+        dump(json_data, json_file, indent=2)
+
+    return True
+# ----------------------------------------------------------------------------------------------- #
