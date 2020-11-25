5058f49774f619f46330a03d1f8e4b5995f3de5b
AngryBaer <angrybaer@users.noreply.github.com>
2020-11-25 00:37:23 -0800

    cleanup

--- abYaml/api.py
+++ abYaml/api.py
@@ -16,11 +16,13 @@ def read(file_path, mode='r'):
     """
     Returns the contents of a single .yml document file.
 
-    :param  file_path:  (str) path to the file
-    :return yaml_data: (dict) data contained in the .yml file
+    :param file_path:  path to the file
+                        - str
+
+    :return yaml_data: data contained in the .yml file
+                        - dict
     """
-    assert os.path.exists(file_path), 'target path does not exist: \n'  \
-                                      '{}'.format(file_path)
+    assert os.path.exists(file_path), 'target path does not exist: \n{}'.format(file_path)
 
     with open(file_path, mode) as yaml_file:
         yaml_data = load(yaml_file, Loader=FullLoader)
@@ -32,11 +34,13 @@ def read_docs(file_path, mode='r'):
     """
     Returns multiple .yml document files.
 
-    :param  file_path:       (str) path to the file
-    :return yaml_docs: (generator) yields each document
+    :param file_path:  path to the file
+                        - str
+
+    :return yaml_docs: generator that yields each document
+                        - generator object
     """
-    assert os.path.exists(file_path), 'target path does not exist: \n'  \
-                                      '{}'.format(file_path)
+    assert os.path.exists(file_path), 'target path does not exist: \n{}'.format(file_path)
 
     with open(file_path, mode) as yaml_file:
         yaml_docs = load_all(yaml_file, Loader=FullLoader)
@@ -50,16 +54,18 @@ def write(file_path, yaml_data, mode='w+'):
     """
     Writes a .yml file containing the given data.
 
-    :param  file_path:  (str) absolute path to file
-    :param  yaml_data: (dict) data contained in the .yml file
-    :return          : (bool) confirmation of transaction
+    :param file_path: absolute path to file
+                       - str
+    :param yaml_data: data contained in the .yml file
+                       - dict
+
+    :return:          confirmation of transaction
+                       - bool
     """
     target_folder = os.path.dirname(file_path)
-    
-    assert os.path.exists(target_folder), 'target folder does not exist: \n'  \
-                                          '{}'.format(target_folder)
-    assert isinstance(yaml_data, dict),   'requires dict data type, given '   \
-                                          '{}'.format(type(yaml_data))
+
+    assert os.path.exists(target_folder), 'target folder does not exist: \n{}'.format(target_folder)
+    assert isinstance(yaml_data, dict),   'requires dict data type, given: {}'.format(type(yaml_data))
 
     with open(file_path, mode) as yaml_file:
         dump(yaml_data, yaml_file)
@@ -71,16 +77,19 @@ def write_docs(file_path, yaml_docs, mode='w+'):
     """
     Writes a .json file containing the given data.
 
-    :param  file_path:       (str) absolute path to file
-    :param  yaml_docs: (list/dict) multiple yaml docs
-    :return          :      (bool) confirmation of transaction
+    :param file_path: absolute path to file
+                       - str
+
+    :param yaml_docs: multiple yaml documents
+                       - list [dict, dict, ...]
+
+    :return:          confirmation of transaction
+                       - bool
     """
     target_folder = os.path.dirname(file_path)
 
-    assert os.path.exists(target_folder), 'target folder does not exist: \n'  \
-                                          '{}'.format(target_folder)
-    assert isinstance(yaml_docs, list),   'requires list data type, given '   \
-                                          '{}'.format(type(yaml_docs))
+    assert os.path.exists(target_folder), 'target folder does not exist: \n{}'.format(target_folder)
+    assert isinstance(yaml_docs, list),   'requires list data type, given: {}'.format(type(yaml_docs))
 
     doc_check = all([isinstance(x, dict) for x in yaml_docs])
     assert doc_check, 'requires all docs to be of type dict'
